879c6800c95a3d8b01556959b29769a7d5603328
devJRL <dev2sponge@gmail.com>
2020-05-16 11:37:46 +0900

    How to use 'selector' for <app-component></app-component> or <div>

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // RECOMMEND!!
+  // DEFAULT SELECTOR for ANGULAR OF TAG : <div app-servers></div> or <div class="app-servers"></div>
+  // selector: 'app-servers',
+
+  // CUSTOM SEELCTOR for ATTRIBUTE IN DIVTAG : <div app-servers></div>
+  selector: '[app-servers]',
+
   template: `
     <app-server></app-server>
     <app-server></app-server>
     `,
+
   // FROM CSS
   styleUrls: ['./servers.component.css']
 })
