b4ebd589ba3a9b6279af32d77e9077361ceeb633
devJRL <dev2sponge@gmail.com>
2020-05-19 22:07:41 +0900

    ngif & else condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -22,6 +22,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: String = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -33,6 +34,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
